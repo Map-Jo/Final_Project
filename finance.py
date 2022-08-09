dbb57bb5a8690f33c6ee90a3b86eb7850940b862
A <105343395+Map-Jo@users.noreply.github.com>
2022-08-09 17:40:39 +0900

    Update finance.py

--- finance.py
+++ finance.py
@@ -4,6 +4,6 @@ import FinanceDataReader as fdr
 
 
 code = st.text_input('Code Num')
-date = st.text_input('Code Num')
+date = st.text_input('Year')
 df = fdr.DataReader(code, date).sort_index(ascending=False)
 df
