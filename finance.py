e768b9a8ffc0ceb25ce4bcab171aa5321779b68e
Map-Jo <ahyun1609@likelion.org>
2022-08-10 00:43:14 +0900

    Update finance.py

--- finance.py
+++ finance.py
@@ -15,6 +15,6 @@ print(df.columns)
 
 col1, col2, col3 = st.columns(3)
 col1.metric("현재 주식가격","%d원" %df['종가'].tail(1)[0], "%d원" %df['종가'].diff().tail(1)[0])
-col2.metric("현재 거래량", df['거래량'].tail(1)[0],"%d %" %round(df['거래량'].pct_change().tail(1)[0] * 100, 2))
+col2.metric("현재 거래량", df['거래량'].tail(1)[0],"%f %" %round(df['거래량'].pct_change().tail(1)[0] * 100, 2))
 col3.metric("전일 대비 가격", round(df['전일대비'].tail(1)[0], 4), round(df['전일대비'].tail(1)[0] * 100, 2))
 
