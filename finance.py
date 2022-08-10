7f51919c62956911b67b6f24d91f463e3ba1fb03
Map-Jo <ahyun1609@likelion.org>
2022-08-10 09:36:26 +0900

    Update finance.py

--- finance.py
+++ finance.py
@@ -18,9 +18,8 @@ if Name in Code_name_list:
     col2.metric("현재 거래량", format(df['거래량'].tail(1)[0], ','),"%.2f%%" %(df['거래량'].pct_change().tail(1)[0] * 100))
     col3.metric("전일 대비 가격", round(df['전일대비'].tail(1)[0], 4), "%.2f%%" %(df['전일대비'].tail(1)[0] * 100))
     fig = plt.figure(facecolor='white', figsize=(20, 10))
-    ax = fig.add_subplot(111)
-    ax.plot(df['종가'])
-    ax.title(Name)
+    plt.plot(df['종가'])
+    plt.title(Name)
     st.pyplot(fig)
 elif Name not in Code_name_list:
     st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
