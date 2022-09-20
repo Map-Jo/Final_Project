54e50cc8bb28e03ce7b2ad3c257fee41da09086f
A <105343395+Map-Jo@users.noreply.github.com>
2022-09-20 23:02:28 +0900

    Update _📊_Today's_Korea_Stock_Market.py

--- pages/_📊_Today's_Korea_Stock_Market.py
+++ pages/_📊_Today's_Korea_Stock_Market.py
@@ -25,21 +25,21 @@ name_list.insert(0, '')
 choice = st.selectbox('검색하실 주식 종목명을 입력해 주세요.',name_list)
 
 
-for i in range(len(name_list)):
-    if choice == name_list[i]:
-        choice_name = Stockcode.loc[Stockcode['Name'] == name_list[i], 'Name'].values
-        choice_name_to_str =np.array2string(choice_name).strip("[]")
-        Name = choice_name_to_str.strip("''")
-Stockcode.set_index('Name', inplace=True)
-Code_name_list = Stockcode.index.tolist()
-with st.spinner('Wait for it...'):
-    if Name in Code_name_list:
-        code_num = Stockcode.at[Name, 'Symbol']
-        df = fdr.DataReader(code_num)
-        col1, col2, col3 = st.columns(3)
-        col1.metric("현재 주식가격",format(df['Close'].tail(1)[0], ',')+'원', "%d원" %(df['Close'].diff().tail(1)[0]))
-        col2.metric("현재 거래량", format(df['Volume'].tail(1)[0], ','),"%.2f%%" %(df['Volume'].pct_change().tail(1)[0] * 100))
-        col3.metric("전일 대비 가격", "%d원" %(df['Close'].diff().tail(1)[0]), "%.2f%%" %(df['Change'].tail(1)[0] * 100))
+# for i in range(len(name_list)):
+#     if choice == name_list[i]:
+#         choice_name = Stockcode.loc[Stockcode['Name'] == name_list[i], 'Name'].values
+#         choice_name_to_str =np.array2string(choice_name).strip("[]")
+#         Name = choice_name_to_str.strip("''")
+# Stockcode.set_index('Name', inplace=True)
+# Code_name_list = Stockcode.index.tolist()
+# with st.spinner('Wait for it...'):
+#     if Name in Code_name_list:
+#         code_num = Stockcode.at[Name, 'Symbol']
+#         df = fdr.DataReader(code_num)
+#         col1, col2, col3 = st.columns(3)
+#         col1.metric("현재 주식가격",format(df['Close'].tail(1)[0], ',')+'원', "%d원" %(df['Close'].diff().tail(1)[0]))
+#         col2.metric("현재 거래량", format(df['Volume'].tail(1)[0], ','),"%.2f%%" %(df['Volume'].pct_change().tail(1)[0] * 100))
+#         col3.metric("전일 대비 가격", "%d원" %(df['Close'].diff().tail(1)[0]), "%.2f%%" %(df['Change'].tail(1)[0] * 100))
 
 #         fig = px.line(df, y='Close', title='{} 종가 Time Series'.format(Name))
 
@@ -68,5 +68,5 @@ with st.spinner('Wait for it...'):
 #         fig2.update_layout(title='{} Candlestick chart'.format(Name))
 #         st.plotly_chart(fig2, use_container_width=True)
 
-    elif Name not in Code_name_list:
-        st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
+#     elif Name not in Code_name_list:
+#         st.text('검색하신 주식 종목이 없습니다. 정확하게 입력해주세요.')
